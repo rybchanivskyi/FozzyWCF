0f89a44c06564858cdd2dff30db452a3b45a0cc4
Victor Rybch <victorrybchanivskyi2@gmail.com>
2020-04-22 19:36:10 +0300

    add files

---
+++ FozzyWCF/FozzyWCFClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using FozzyWCF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FozzyWCFHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            Type serviceType = typeof(CelsiusToFahrenheit);
+            using (ServiceHost host = new ServiceHost(serviceType))
+            {
+                host.Open();
+
+                Console.WriteLine("Host Started!");
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ FozzyWCF/FozzyWCFClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("FozzyWCFClient")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FozzyWCFClient")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("d614f6c2-654b-4997-9478-4b01c686de04")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
