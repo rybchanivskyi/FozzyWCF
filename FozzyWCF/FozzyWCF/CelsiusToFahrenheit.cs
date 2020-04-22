4b1079309e82c9d35f44a50ac143327e3a5f95ad
rybchanivskyi <46498071+rybchanivskyi@users.noreply.github.com>
2020-04-22 18:57:16 +0300

    Add files via upload

---
+++ FozzyWCF/FozzyWCF/CelsiusToFahrenheit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace FozzyWCF
+{
+    public class CelsiusToFahrenheit : ICelsiusToFahrenheit
+    {
+        public double ConvertToFahrenheit(double celsius)
+        {
+            Console.WriteLine($"Start converting {celsius} Celsius to Fahrenheit");
+            return (celsius * 9 / 5) + 32;
+        }
+    }
+}
---
+++ FozzyWCF/FozzyWCF/ICelsiusToFahrenheit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace FozzyWCF
+{
+    [ServiceContract]
+    public interface ICelsiusToFahrenheit
+    {
+        [OperationContract]
+        double ConvertToFahrenheit(double celsius);
+    }
+}
